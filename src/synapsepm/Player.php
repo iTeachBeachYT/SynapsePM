c081b40910414f12a4f799e9b779f83f80581300
Honza Bednář <honzaxp01@gmail.com>
2019-07-21 17:32:36 +0200

    Update synapse to 1.12

--- src/synapsepm/Player.php
+++ src/synapsepm/Player.php
@@ -4,16 +4,16 @@ declare(strict_types=1);
 namespace synapsepm;
 
 use pocketmine\level\Level;
-use pocketmine\network\mcpe\protocol\AvailableEntityIdentifiersPacket;
+use pocketmine\network\mcpe\protocol\ActorEventPacket;
+use pocketmine\network\mcpe\protocol\AvailableActorIdentifiersPacket;
 use pocketmine\network\mcpe\protocol\DataPacket;
-use pocketmine\network\mcpe\protocol\EntityEventPacket;
-use pocketmine\network\mcpe\protocol\FullChunkDataPacket;
+use pocketmine\network\mcpe\protocol\LevelChunkPacket;
 use pocketmine\network\mcpe\protocol\MobEffectPacket;
-use pocketmine\network\mcpe\protocol\MoveEntityAbsolutePacket;
+use pocketmine\network\mcpe\protocol\MoveActorAbsolutePacket;
 use pocketmine\network\mcpe\protocol\PlayStatusPacket;
 use pocketmine\network\mcpe\protocol\ProtocolInfo;
 use pocketmine\network\mcpe\protocol\ResourcePacksInfoPacket;
-use pocketmine\network\mcpe\protocol\SetEntityMotionPacket;
+use pocketmine\network\mcpe\protocol\SetActorMotionPacket;
 use pocketmine\network\mcpe\protocol\StartGamePacket;
 use pocketmine\Player as PMPlayer;
 use pocketmine\utils\UUID;
@@ -63,10 +63,8 @@ class Player extends PMPlayer {
     public function forceSendEmptyChunks() {
         foreach ($this->usedChunks as $index => $true) {
             Level::getXZ($index, $chunkX, $chunkZ);
-            $pk = new FullChunkDataPacket();
-            $pk->chunkX = $chunkX;
-            $pk->chunkZ = $chunkZ;
-            $pk->data = '';
+
+            $pk = LevelChunkPacket::withoutCache($chunkX, $chunkZ, 16, '');
             $this->dataPacket($pk);
         }
     }
@@ -119,7 +117,7 @@ class Player extends PMPlayer {
                 return true;
             }
 
-            if ($packet instanceof StartGamePacket || $packet instanceof AvailableEntityIdentifiersPacket) {
+            if ($packet instanceof StartGamePacket || $packet instanceof AvailableActorIdentifiersPacket) {
                 return true;
             }
         } else {
@@ -147,7 +145,7 @@ class Player extends PMPlayer {
     }
 
     public function broadcastEntityEvent(int $eventId, ?int $eventData = \null, ?array $players = \null): void {
-        $pk = new EntityEventPacket();
+        $pk = new ActorEventPacket();
         $pk->entityRuntimeId = $this->id;
         $pk->event = $eventId;
         $pk->data = $eventData ?? 0;
@@ -164,7 +162,7 @@ class Player extends PMPlayer {
     }
 
     protected function broadcastMotion(): void {
-        $pk = new SetEntityMotionPacket();
+        $pk = new SetActorMotionPacket();
         $pk->entityRuntimeId = PHP_INT_MAX;
         $pk->motion = $this->getMotion();
 
@@ -174,7 +172,7 @@ class Player extends PMPlayer {
     }
 
     protected function broadcastMovement(bool $teleport = false): void {
-        $pk = new MoveEntityAbsolutePacket();
+        $pk = new MoveActorAbsolutePacket();
         $pk->entityRuntimeId = PHP_INT_MAX;
         $pk->position = $this->getOffsetPosition($this);
 
@@ -186,7 +184,7 @@ class Player extends PMPlayer {
         $pk->zRot = $this->yaw;
 
         if ($teleport) {
-            $pk->flags |= MoveEntityAbsolutePacket::FLAG_TELEPORT;
+            $pk->flags |= MoveActorAbsolutePacket::FLAG_TELEPORT;
         }
 
         $this->sendDataPacket($pk);
@@ -194,14 +192,14 @@ class Player extends PMPlayer {
         parent::broadcastMovement($teleport);
     }
 
-    public function handleEntityEvent(EntityEventPacket $packet): bool {
+    public function handleEntityEvent(ActorEventPacket $packet): bool {
         if (!$this->spawned or !$this->isAlive()) {
             return true;
         }
         $this->doCloseInventory();
 
         switch ($packet->event) {
-            case EntityEventPacket::EATING_ITEM:
+            case ActorEventPacket::EATING_ITEM:
                 if ($packet->data === 0) {
                     return false;
                 }
